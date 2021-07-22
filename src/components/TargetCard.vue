5d89a1fc3539beb62070469acc38bc63a0aad02e
Katie Ciurleo <katieciurleo@gmail.com>
2021-07-22 09:11:43 -0700

    best guess images

--- src/components/TargetCard.vue
+++ src/components/TargetCard.vue
@@ -4,7 +4,7 @@
       <div class="card-content">
         <h2 class="is-size-4 has-text-weight-bold">{{ target.name }}</h2>
         <h3 class="target-type">{{ target.type }}</h3>
-        <img src="@/assets/DefaultTargetImages/SextansC_1_E_GLC_SDSS9.jpg"/>
+        <img :src="`/assets/DefaultTargetImages/${target.image}`"/>
         <h4>{{ target.image }}</h4>
       </div>
     </div>
